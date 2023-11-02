07c1ac5f00738fa48c89bf2e2f09261117126f99
Trần Minh Quân <100234784+Tranquandev@users.noreply.github.com>
2023-11-02 12:04:36 +0700

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className='grid grid-cols-1 gap-4 px-4 lg:grid-cols-3'>
           <div className='lg:col-span-1'>
             <div className='text-text-tt-ft'>
-              © 2022 Shopee. Tất cả các quyền được bảo lưu.
+              © 2022 lorem. Tất cả các quyền được bảo lưu.
             </div>
           </div>
           <div className='lg:col-span-2'>
@@ -18,20 +18,17 @@ export default function Footer() {
         <div className='mt-10 px-4 text-center text-sm'>
           <div className='text-text-tt-ft'>Công ty TNHH Shopee</div>
           <div className='mt-6 text-text-tt-ft'>
-            Địa chỉ: Tầng 4-5-6, Tòa nhà Capital Place, số 29 đường Liễu Giai,
-            Phường Ngọc Khánh, Quận Ba Đình, Thành phố Hà Nội, Việt Nam. Tổng
-            đài hỗ trợ: 19001221 - Email: cskh@hotro.shopee.vn
+            Địa chỉ: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 15000.
           </div>
           <div className='mt-2 text-text-tt-ft'>
             Chịu Trách Nhiệm Quản Lý Nội Dung: Trần Minh Quân - Điện thoại liên
             hệ: 0798038589
           </div>
           <div className='mt-2 text-text-tt-ft'>
-            Mã số doanh nghiệp: 0106773786 do Sở Kế hoạch & Đầu tư TP Hà Nội cấp
-            lần đầu ngày 10/02/2015
+            Mã số doanh nghiệp: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
           </div>
           <div className='mt-2 text-text-tt-ft'>
-            © 2015 - Bản quyền thuộc về Công ty TNHH Shopee
+            © 2015 - Bản quyền thuộc về MinhQuan
           </div>
         </div>
       </div>
